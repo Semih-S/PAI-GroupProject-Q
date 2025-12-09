c996557d33435be606facce04653e1493001334d
flower-forever <158334651+flower-forever@users.noreply.github.com>
2025-12-09 14:43:02 -0300

    Refactor database connection code structure

--- PAI-GroupProject-Q/src/Student_Wellbeing_App/core/database/connection.py
+++ PAI-GroupProject-Q/src/Student_Wellbeing_App/core/database/connection.py
@@ -17,4 +17,7 @@ def get_db_connection() -> sqlite3.Connection:
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     conn.execute("PRAGMA foreign_keys = ON")
+
     return conn
+
+get_db_pool = get_db_connection
