e40cb6122b1a3bb9b665dff20c87c9fc08a4602c
Onkar2003-lab <onkargupta45@gmail.com>
2025-12-09 18:43:46 +0530

    Add comments to Student class for clarity
    Added comments to clarify the purpose of the Student class and its properties.

--- PAI-GroupProject-Q/src/Student_Wellbeing_App/core/models/Student.py
+++ PAI-GroupProject-Q/src/Student_Wellbeing_App/core/models/Student.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
 
-
+# Store key personal and academic identify information
 @dataclass
 class Student:
+    # identifier for each student
     student_id: str
     first_name: str
     lastname: str
@@ -13,5 +14,7 @@ class Student:
 
     @property
     def full_name(self) -> str:
+        # returns the student full name is a readable format
         return f"{self.first_name} {self.lastname}"
+
 
